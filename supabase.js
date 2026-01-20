5850a5e63f290fabf239171a416acadd4271d989
omdy9 <149556859+omdy9@users.noreply.github.com>
2026-01-20 18:11:19 +0530

    Add Supabase client initialization script

---
+++ supabase.js
@@ -0,0 +1,9 @@
+<script>
+const SUPABASE_URL = "https://mcvfdjrbcwnfhrsuquil.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jdmZkanJiY3duZmhyc3VxdWlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5MDg3MjgsImV4cCI6MjA4NDQ4NDcyOH0.HB-SFOE_XuZKXMn7gVzsO8EY9rgPpW9ws3m-QVS0lmI";
+
+const supabase = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
+</script>
